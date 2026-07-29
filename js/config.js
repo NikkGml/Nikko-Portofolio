c6459034f59200df040d60027b2fbf993455e697
NikkGml <efendisetiaji07@gmail.com>
2026-07-29 21:41:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -55,8 +55,8 @@ const CONFIG_STATS = [
    ---------------------------------------------------------------- */
 const CONFIG_SOCIAL = {
   email: "efendisetiaji07@gmail.com",
-  instagram: "https://www.instagram.com/nikkgml/",   // kosongkan "" untuk sembunyikan
-  github: "https://github.com/NikkGml" .NikkGml,
+  instagram: "https://www.instagram.com/nikkgml/" .NikkGml,   // kosongkan "" untuk sembunyikan
+  github: "https://github.com/NikkGml",
   linkedin: "www.linkedin.com/in/febian-nikko-ferdi-ansyah-b26879384",
   twitter: "",   // kosongkan "" untuk sembunyikan
 };
